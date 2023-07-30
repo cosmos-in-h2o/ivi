c857b028ab08ed651ed7f8512aa17e87b7b196dd
makise-curisu <dream77070@163.com>
2023-07-30 17:02:20 +0800

    update

--- lua/core/setup.lua
+++ lua/core/setup.lua
@@ -3,3 +3,7 @@ require("config.keymaps")
 --设置主题
 require("config.colorscheme")
 require("config.cmp")
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = { "*.ixx", "*.cppm" },
+	command = "set filetype=cpp",
+})
